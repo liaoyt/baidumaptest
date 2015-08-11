771a3c950d062e36943604e3f9bb4db180df8e67
LiaoYT <liaoyt66066@gmail.com>
2015-08-11 16:49:31 +0800

    First commit

---
+++ src/com/example/baidumaptest/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.baidumaptest;
+
+import java.util.List;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.example.baidumaptest.R.id;
+
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	private MapView mapView;
+	
+	private BaiduMap baiduMap;
+
+	private LocationManager locationManager;
+	
+	private String provider;
+	private boolean isFirstLocate = true;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		SDKInitializer.initialize(getApplicationContext());
+		setContentView(R.layout.activity_main);
+		
+		mapView = (MapView)findViewById(R.id.map_view);
+		baiduMap = mapView.getMap();
+		baiduMap.setMyLocationEnabled(true);
+		
+		locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+		List<String> providers = locationManager.getProviders(true);
+		if (providers.contains(LocationManager.GPS_PROVIDER)) {
+			provider = LocationManager.GPS_PROVIDER;
+		}else if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
+			provider = LocationManager.NETWORK_PROVIDER;
+		}else {
+			Toast.makeText(this, "No location provider to use", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		Location location = locationManager.getLastKnownLocation(provider);
+		if (location != null) {
+			navigateTo(location);
+		}
+		locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
+	}
+	
+	private void navigateTo(Location location) {
+		// TODO Auto-generated method stub
+		if (isFirstLocate) {
+			LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+			MapStatusUpdate update = MapStatusUpdateFactory.newLatLng(latLng);
+			baiduMap.animateMapStatus(update);
+			update = MapStatusUpdateFactory.zoomTo(16f);
+			baiduMap.animateMapStatus(update);
+			isFirstLocate = false;
+		}
+		
+		MyLocationData.Builder locationBuilder = new MyLocationData.Builder();
+		locationBuilder.latitude(location.getLatitude());
+		locationBuilder.longitude(location.getLongitude());
+		MyLocationData locationData = locationBuilder.build();
+		baiduMap.setMyLocationData(locationData);
+	}
+	
+	LocationListener locationListener = new LocationListener() {
+		
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onLocationChanged(Location location) {
+			// TODO Auto-generated method stub
+			if(location != null){
+				navigateTo(location);
+			}
+		}
+	};
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		baiduMap.setMyLocationEnabled(false);
+		mapView.onDestroy();
+		if (locationManager != null) {
+			locationManager.removeUpdates(locationListener);
+		}
+	}
+	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		mapView.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mapView.onResume();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
